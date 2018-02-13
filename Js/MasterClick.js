af7f575198b25f87e9447e234e886ef1b83352fe
akash gawande <akashgawande@gmail.com>
2018-02-13 17:28:04 +0530

    Js

---
+++ Js/MasterClick.js
@@ -0,0 +1,132 @@
+﻿$(document).ready(function () {
+});
+
+
+
+function ActiveClass(form)
+{
+    if (form == "Dashboard") {
+        $('#GstReg').prop('class', '')
+        $("[id*=liDash]").prop('class', 'active')
+        $('#ITFiles').prop('class', '')
+        $('#TDSFiles').prop('class', '')
+        $('#ITNFiles').prop('class', '')
+        $("[id*=liequi]").prop('class', '')
+        $("[id*=liClient]").prop('class', '')
+        $('#Employee').prop('class', '')
+        $('#Franchisee').prop('class', '')
+        $('#Works').prop('class', '')
+    }
+else if(form=="GSTReg")
+{
+    $('#GstReg').prop('class', 'active')
+    $("[id*=liDash]").prop('class', '')
+    $('#ITFiles').prop('class', '')
+    $('#TDSFiles').prop('class', '')
+    $('#ITNFiles').prop('class', '')
+    $("[id*=liequi]").prop('class', '')
+    $("[id*=liClient]").prop('class', '')
+    $('#Employee').prop('class', '')
+    $('#Franchisee').prop('class', '')
+    $('#Works').prop('class', '')
+}
+else if (form == "IT") {
+    $('#GstReg').prop('class', '')
+    $("[id*=liDash]").prop('class', '')
+    $('#ITFiles').prop('class', 'active')
+    $('#TDSFiles').prop('class', '')
+    $('#ITNFiles').prop('class', '')
+    $("[id*=liequi]").prop('class', '')
+    $("[id*=liClient]").prop('class', '')
+    $('#Employee').prop('class', '')
+    $('#Franchisee').prop('class', '')
+    $('#Works').prop('class', '')
+}
+else if (form == "TDS") {
+    $('#GstReg').prop('class', '')
+    $("[id*=liDash]").prop('class', '')
+    $('#ITFiles').prop('class', '')
+    $('#TDSFiles').prop('class', 'active')
+    $('#ITNFiles').prop('class', '')
+    $("[id*=liequi]").prop('class', '')
+    $("[id*=liClient]").prop('class', '')
+    $('#Employee').prop('class', '')
+    $('#Franchisee').prop('class', '')
+    $('#Works').prop('class', '')
+}
+else if (form == "ITN") {
+    $('#GstReg').prop('class', '')
+    $("[id*=liDash]").prop('class', '')
+    $('#ITFiles').prop('class', '')
+    $('#TDSFiles').prop('class', '')
+    $('#ITNFiles').prop('class', 'active')
+    $("[id*=liequi]").prop('class', '')
+    $("[id*=liClient]").prop('class', '')
+    $('#Employee').prop('class', '')
+    $('#Franchisee').prop('class', '')
+    $('#Works').prop('class', '')
+}
+else if (form == "Inbox") {
+    $('#GstReg').prop('class', '')
+    $("[id*=liDash]").prop('class', '')
+    $('#ITFiles').prop('class', '')
+    $('#TDSFiles').prop('class', '')
+    $('#ITNFiles').prop('class', '')
+    $("[id*=liequi]").prop('class', 'active')
+    $("[id*=liClient]").prop('class', '')
+    $('#Employee').prop('class', '')
+    $('#Franchisee').prop('class', '')
+    $('#Works').prop('class', '')
+}
+   
+else if (form == "Client") {
+    $('#GstReg').prop('class', '')
+    $("[id*=liDash]").prop('class', '')
+    $('#ITFiles').prop('class', '')
+    $('#TDSFiles').prop('class', '')
+    $('#ITNFiles').prop('class', '')
+    $("[id*=liequi]").prop('class', '')
+    $("[id*=liClient]").prop('class', 'active')
+    $('#Employee').prop('class', '')
+    $('#Franchisee').prop('class', '')
+    $('#Works').prop('class', '')
+}
+else if (form == "Employee") {
+    $('#GstReg').prop('class', '')
+    $("[id*=liDash]").prop('class', '')
+    $('#ITFiles').prop('class', '')
+    $('#TDSFiles').prop('class', '')
+    $('#ITNFiles').prop('class', '')
+    $("[id*=liequi]").prop('class', '')
+    $("[id*=liClient]").prop('class', '')
+    $('#Employee').prop('class', 'active')
+    $('#Franchisee').prop('class', '')
+    $('#Works').prop('class', '')
+}
+else if (form == "Franchisee") {
+    $('#GstReg').prop('class', '')
+    $("[id*=liDash]").prop('class', '')
+    $('#ITFiles').prop('class', '')
+    $('#TDSFiles').prop('class', '')
+    $('#ITNFiles').prop('class', '')
+    $("[id*=liequi]").prop('class', '')
+    $("[id*=liClient]").prop('class', '')
+    $('#Employee').prop('class', '')
+    $('#Franchisee').prop('class', 'active')
+    $('#Works').prop('class', '')
+}
+else if (form == "Workallocation") {
+    $('#GstReg').prop('class', '')
+    $("[id*=liDash]").prop('class', '')
+    $('#ITFiles').prop('class', '')
+    $('#TDSFiles').prop('class', '')
+    $('#ITNFiles').prop('class', '')
+    $("[id*=liequi]").prop('class', '')
+    $("[id*=liClient]").prop('class', '')
+    $('#Employee').prop('class', '')
+    $('#Franchisee').prop('class', '')
+    $('#Works').prop('class', 'active')
+}
+
+
+}
---
+++ Js/MasterJs.js
@@ -0,0 +1,51 @@
+﻿/// <reference path="../assets/js/jquery-2.1.4.min.js" />
+
+
+var totalmessagecount = 0;
+$(document).ready(function () {
+    ShowUnreadMsgDetl();
+});
+
+
+//--------------------------Start Show Unread Message Data-------------------------------------------
+function ShowUnreadMsgDetl() {
+
+    $("#tblUnreadmsgList tr").remove();
+    var count = 0;
+    var countunread = 0;
+    totalmessagecount = 0;
+    $.ajax({
+        type: "post",
+        url: "Dashboard.aspx/ShowMsg",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            $.each(response.d, function (index, value) {
+                count = count + 1
+                totalmessagecount = totalmessagecount + 1
+                console.log(response);
+
+                var rows = "";
+                rows += "<tr>"
+                        + "<td class='customertd' style='text-align:center;Display:none'>" + count + "</td>"
+                        + "<td class='customertd' style='Display:none'>" + value.Id + "</td>"
+                        + "<td class='customertd'>"
+                        + "<a href='Inbox.aspx' id='OpenMsg' class='clearfix'><img src='../assets/images/avatars/avatar.png' class='msg-photo' alt='Alex's Avatar' /><span class='msg-body'><span class='msg-title'><span class='blue' >" + value.Sender_Name + ":</span>" + value.Subject + " ...</span><span class='msg-time'><i class='ace-icon fa fa-clock-o'></i><span>" + value.Date +"</span></span></span></a>"
+                        + "</td>"
+                        + "</tr>";
+
+                $('#tblUnreadmsgList').append(rows);
+
+
+                //$('#lblmessagecount').text(totalmessagecount + "  Messages Total");
+            });
+            $('#unreadmsgcount').text(count);
+            $('#Messagecount').text(count + " Unread Messages");
+
+        },
+        failure: function () {
+        }
+
+    });
+}
+//--------------------------Start Show Unread Message Data-------------------------------------------
